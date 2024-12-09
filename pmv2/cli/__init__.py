e2730f651f0e384339f19a0f310c7e91a0cacd1b
Aleksei Sokol <kanootoko@gmail.com>
2024-12-09 08:41:10 +0300

    Version 0.6.0: analyze urban objects intersections
    Changes:
    - add analyze urban objects intersections logic
    - add update urban objects object_geometry_id logic

--- pmv2/cli/__init__.py
+++ pmv2/cli/__init__.py
@@ -1,7 +1,12 @@
 """Click configuration is performed here."""
 
-from . import _list, _pickle, _upload_buildings, _upload_functional_zones, _upload_physical_objects, _upload_services
+import importlib
+from pathlib import Path
 from ._main import main
+
+for file in Path(__file__).resolve().parent.glob("*.py"):
+    if file.name != "__init__.py":
+        importlib.import_module(f"pmv2.cli.{file.name[:-3]}")
 
 __all__ = [
     "main",
